843f03b3b30fc9d27da965402f52fd5d90b3564e
capehill <juha.niemimaki@gmail.com>
2026-09-02 21:31:22 +0300

    Log OpenGL library versions

--- src/video/amigaos4/SDL_os4minigl.c
+++ src/video/amigaos4/SDL_os4minigl.c
@@ -74,7 +74,7 @@ OS4_MiniGL_LoadLibrary(SDL_VideoDevice *_this, const char * path)
         return SDL_SetError("Failed to open MiniGL interface");
     }
 
-    dprintf("MiniGL library opened\n");
+    dprintf("MiniGL library %d.%d opened\n", MiniGLBase->lib_Version, MiniGLBase->lib_Revision);
     return true;
 }
 
